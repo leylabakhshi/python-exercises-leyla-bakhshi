ad29025d8bbfe92b59d3c81b9761b8b50ebf6c3e
leylabakhshi <leylabakhshi7171@gmail.com>
2026-08-04 20:15:50 +0330

    Add new work

---
+++ New work/untitled1.py
@@ -0,0 +1,17 @@
+e=int(input('enter age:'))
+g=input('enter gensiat:')
+if g=='m' and e>=0 and e<=40:
+    print('mr')
+elif g=='m' and e>=40 and e<60:
+    print('pedar')
+elif g=='m'  and e>=60:
+    print('grand father')
+elif g=='w' and e>=0 and e<=40:
+    print('miss')
+elif g=='w' and e>=40 and e<60:
+    print('mother')
+elif g=='w' and e>=60:
+    print('grand mother')
+input('press enter to exit...')    
+    
+
